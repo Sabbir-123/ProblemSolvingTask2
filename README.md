6fcc78c5ac432aabf74f6f7dcd10a98ca1589f8e
Sabbir-123 <ahmedsabbir2013@gmail.com>
2023-07-17 22:01:46 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ProblemSolvingTask2
---
+++ index.js
@@ -0,0 +1,15 @@
+function isPalindrome(str) {
+  // Remove punctuation, convert to lowercase, and remove spacing
+  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]+/g, '');
+
+  // Reverse the formatted string
+  const reversedStr = formattedStr.split('').reverse().join('');
+
+  // Check if the formatted string and its reverse are equal
+  return formattedStr === reversedStr;
+}
+
+
+console.log(isPalindrome("level")); 
+console.log(isPalindrome("hello")); 
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
